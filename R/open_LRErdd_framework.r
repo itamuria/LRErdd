874dff05e89243ac7d26e25651ae37ea47de6df3
itamuria <itamuria@gmail.com>
2018-09-18 10:01:08 -0400

    open form

---
+++ R/open_LRErdd_framework.r
@@ -0,0 +1,12 @@
+#' Example of LRErdd in shiny version
+#'
+#' @export
+
+open_LRErdd_framework <- function() {
+  appDir <- system.file("shiny-examples", "LRErdd", package = "LRErdd")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
+  }
+  
+  shiny::runApp(appDir, display.mode = "normal")
+}
